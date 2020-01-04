7fe6137c63b2f858e2df2d7143d9a60cf9f7b835
Peter Dziezyk <dnaspider@live.com>
2020-01-04 13:19:21 -0500

    Implemented se.txt hot reload variant
    //se.txt
    SeHotReload_CtrlS: 1
    
    Hot reload se.txt (settings) when pressing ctrl+s to save; Optional.

--- dnaspider.cpp
+++ dnaspider.cpp
@@ -73,6 +73,7 @@ string OutsTemplate = "strand: ";
 bool EscCommaAutoBs = true;
 string code = "";
 bool EscHAutoBs = true;
+bool SeHotReload_CtrlS=1;
 #pragma endregion
 
 #pragma region global_sub
@@ -322,6 +323,7 @@ void loadSe() {
 		if (se == "CommaSleep:") { CommaSleep = stoi(v); continue; }
 		if (se == "StockInterfaceControls:") { StockInterfaceControls = stoi(v); continue; }
 		if (se == "AutoBs_EscH:") { EscHAutoBs = stoi(v); continue; }
+		if (se == "SeHotReload_CtrlS:") { SeHotReload_CtrlS = stoi(v); continue; }
 		if (se == "Exit_EscX:") { enableEscX = stoi(v); continue; }	//}catch (const std::exception&){cout << "Error in " << settings << "!\n";};
 	}
 	f.close();
@@ -411,6 +413,7 @@ void printSe() {
 		cout << "CommaSleep: " << CommaSleep << endl;
 		cout << "StockInterfaceControls: " << StockInterfaceControls << endl;
 		cout << "AutoBs_EscH: " << EscHAutoBs << endl;
+		cout << "SeHotReload_CtrlS: " << SeHotReload_CtrlS << endl;
 		cout << "Exit_EscX: " << enableEscX << endl;
 		cout << endl;
 	}
@@ -860,7 +863,7 @@ int main() {//cout << "@dnaspider\n\n";
 	printIntro();
 #pragma endregion
 	for (;; Sleep(frequency)) {
-		if ((GetAsyncKeyState(VK_LCONTROL) && GetAsyncKeyState(83)) && (FindWindowA(0, "se.txt - Notepad") == GetForegroundWindow() || FindWindowA(0, "se.txt - Visual Studio Code") == GetForegroundWindow())) { Sleep(150); HWND np = FindWindowA(0, "se.txt - Notepad"); HWND vsc = FindWindowA(0, "se.txt - Visual Studio Code"); HWND hotreload = GetForegroundWindow(); if (np == hotreload || vsc == hotreload) { loadSe(); strand.clear(); continue; } }//lctrl+s hot reload
+		if ((SeHotReload_CtrlS) && (GetAsyncKeyState(VK_LCONTROL) && GetAsyncKeyState(83)) && (FindWindowA(0, "se.txt - Notepad") == GetForegroundWindow() || FindWindowA(0, "se.txt - Visual Studio Code") == GetForegroundWindow())) { Sleep(150); HWND np = FindWindowA(0, "se.txt - Notepad"); HWND vsc = FindWindowA(0, "se.txt - Visual Studio Code"); HWND HotReload = GetForegroundWindow(); if (np == HotReload || vsc == HotReload) { loadSe(); if (SeDbClearStrand_CtrlS) strand.clear(); else if (!ignoreAZ) key("s"); continue; } }//lctrl+s hot reload
 		if (GetAsyncKeyState(VK_BACK)) {
 			strand = strand.substr(0, strand.length() - 1);
 			prints(); continue;
