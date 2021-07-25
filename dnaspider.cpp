bb4f0064a654762bd0bf7804f7cd9e572a804fa6
Peter Dziezyk <dnaspider@live.com>
2021-07-25 10:44:49 -0400

    MultiStrand variant

--- dnaspider.cpp
+++ dnaspider.cpp
@@ -1336,7 +1336,7 @@ void scanDb() {
 				break;
 			case '>':
 				tail = tail.substr(1, tail.length());
-				codes = strand[0] == '<' ? strand.substr(1, strand.length()) + tail : strand + tail;
+				if (sv > L"") codes = strand[0] == '<' ? strand.substr(1, strand.length()) + tail : strand + tail;
 				break;
 			case '-':
 				tail = tail.substr(1, tail.length());
@@ -1347,12 +1347,13 @@ void scanDb() {
 			default:
 				codes = cell;
 			}
-			if (multiStrand) { reTail = tail; mainn.setTail(); }
+			if (multiStrand) { reTail = codes; mainn.setTail(); }
 			if (showOuts && !showMultiStrand) { showOutsMsg(L"found: " + OutTab + OutTab, cell); showOutsMsg(L"tail: " + OutTab + OutTab, tail); }
 			if (tail.find(L"<rp>") != std::string::npos) { POINT pt; GetCursorPos(&pt); qxc = pt.x; qyc = pt.y; }
 			f.close(); fail = 0; esc_pressed = 0;
 			Multi multi;
 			for (i = 0; i < tail.length(); ++i) {
+				if (multiStrand) if (strand > L"" && strand == sv && !noClearStrand) strand.clear();//multiStrand variant clear
 				if (speed > 0) { if (sleep) { if (multiStrand) { multi.t = tail; multi.get_i = i; multi.q = qq; } Sleep(speed); if (multiStrand) { tail = multi.t; i = multi.get_i; qq = multi.q; } } sleep = 1; }
 				GetAsyncKeyState(VK_ESCAPE); if (GetAsyncKeyState(VK_ESCAPE) || esc_pressed) { esc_pressed = 0; pause_resume = 0; multiblock = 0; break; } if (GetAsyncKeyState(VK_PAUSE)) { if (pause_resume) { pause_resume = 0; GetAsyncKeyState(VK_PAUSE); kbRelease(VK_PAUSE); } else { pause_resume = 1; } }// int m = MessageBoxA(0, "Resume?", "dnaspider", MB_YESNO); if (m != IDYES) { break; } }
 				if (pause_resume) { --i; Sleep(frequency); continue; }
@@ -2734,7 +2735,7 @@ void scanDb() {
 			if (strand > L"" || re > L"") {
 				if (re == L"" || re == L" " || strandLengthMode) { re = L""; reTail = tail = codes; }
 				clearAllKeys();
-				if (!noClearStrand) strand.clear();
+				if (!noClearStrand && strand == sv) strand.clear();
 			}
 			if (noClearStrand && !close_ctrl_mode) noClearStrand = 0;
 			if (speed > 0) speed = 0;
