2948e45d2da9ed6b662bb7b39c0f3580c5768683
PD <dnaspider@live.com>
2024-01-31 16:22:22 -0500

    `°·.(▀Ĺ̯▀ ̿).·°´

--- dnaspider.cpp
+++ dnaspider.cpp
@@ -3321,11 +3321,11 @@ ShowStrand          0
 RSHIFT+LSHIFT_Only  0 0
 CtrlScanOnlyMode    0
 Kb_Key_Comma        >, '<bs>
-CtrlKey             163 9
+CtrlKey             163 9		
 RgbScaleLayout      2.5)";
-					np = L"";
+					np = L"";	
 					Kb_Key_Comma = L">, '<bs>"; RgbScaleLayout = 2.5; strandLengthMode = 1; cKey = VK_RCONTROL; cKeyMax = 9; RSHIFTLSHIFT_Only = 0; qScanOnly = false;
-					Sleep(250); kbRelease(VK_ESCAPE); GetAsyncKeyState(VK_ESCAPE);
+					Sleep(2048); kbRelease(VK_ESCAPE); GetAsyncKeyState(VK_ESCAPE);
 				}
 				wofstream fd(database); fd.imbue(locale(fd.getloc(), new codecvt_utf8_utf16<wchar_t>)); fd << db_; fd.close(); wofstream fs(settings); fs.imbue(locale(fs.getloc(), new codecvt_utf8_utf16<wchar_t>)); fs << se_; fs.close(); out(L"<win>r<win-><app: run, 3, 60, :>" + np + settings + L"<enter><ms: 1500><win>r<win-><app: run, 3, 60, :>" + np + database + L"<enter>"); re.clear(); tail.clear(); strand.clear();
 			}
