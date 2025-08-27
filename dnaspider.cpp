a626a4c00b10dbab2e679eedb46af53c688cd4d4
Peter Dziezyk <dnaspider@live.com>
2025-08-26 20:44:45 -0400

    ♾️`°·.(▀Ĺ̯▀ ̿).·°´♾️

--- dnaspider.cpp
+++ dnaspider.cpp
@@ -3798,8 +3798,8 @@ RgbScaleLayout			1.0)";
 					}
 					++rri; if (strand[0] || rri > 1) {
 						if (strand[0] == '<' && strand.length() > 1 || RSHIFTLSHIFT_Only == 2 && strand[0] && strand != L"<") {
-							rri = 0; strand.append(L">"); prints(); if (!multiStrand) { scanDb(); if (!noClearStrand) { strand.clear(); } noClearStrand = 0; prints(); continue; }
-							else { i = -1; thread thread(scanDb); Sleep(CloseCtrlSpacer); if (!noClearStrand) { strand.clear(); } noClearStrand = 0; thread.detach(); }
+							if (strand[0] && x == 0) { key(L">"); continue; }
+							else strand = L"<";
 						}
 						else if (strand[0] != '<') {
 							if (strand[0] && x == 0) { key(L">"); continue; }
